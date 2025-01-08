4e9a808d2e9540542e090259c8125693e68b182e
darwynkjoy <darwynkjoy@gmail.com>
2025-01-08 13:22:40 +0530

    daily push

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -38,53 +38,54 @@ class _homepageState extends State<Homepage>{
         backgroundColor: Colors.blue,
       ),
       body: ListView.separated(itemBuilder: (context,index){
-        return Container(
-          height: 220,
-          child: Card(
-            elevation: 10,
-            color: const Color.fromARGB(255, 220, 239, 255),
-            shadowColor: Colors.black,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Title                           :",style: TextStyle(fontSize: 18,color: Colors.black),),
-                  Text("Author                       :",style: TextStyle(fontSize: 18,color: Colors.black),),
-                  Text("Genere                      :",style: TextStyle(fontSize: 18,color: Colors.black),),
-                  Text("Year of publication :",style: TextStyle(fontSize: 18,color: Colors.black),),
-                  Text("Rating                       :",style: TextStyle(fontSize: 18,color: Colors.black),),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                          backgroundColor: Colors.blue,
-                        ),
-                        onPressed: (){}, child: Icon(Icons.edit,color: Colors.white,)),
-                       ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                          backgroundColor: Colors.blue,
-                        ),
-                        onPressed: (){}, child: Icon(Icons.delete,color: Colors.white,))
-                    ],
-                  ),
-                ],
-              ),
+        final books=bookItems[index];
+        return Card(
+          elevation: 10,
+          color: const Color.fromARGB(255, 220, 239, 255),
+          shadowColor: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Title                           :${books["title"]}",style: TextStyle(fontSize: 18,color: Colors.black),),
+                Text("Author                       :${books["Author"]}",style: TextStyle(fontSize: 18,color: Colors.black),),
+                Text("Genere                      :${books["Genere"]}",style: TextStyle(fontSize: 18,color: Colors.black),),
+                Text("Year of publication :${books["year"]}",style: TextStyle(fontSize: 18,color: Colors.black),),
+                Text("Rating                       :${books["Rating"]}",style: TextStyle(fontSize: 18,color: Colors.black),),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    PopupMenuButton(
+                      iconColor: Colors.black,
+                      itemBuilder: (BuildContext context){
+                      return [
+                        PopupMenuItem(
+                          child: TextButton.icon(onPressed: (){},
+                          label: Text("Edit",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blue),),)
+                          ),
+                        PopupMenuItem(
+                          child: TextButton.icon(onPressed: (){},
+                          label: Text("Delete",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.blue),),)
+                          ),
+                      ];
+                    }),
+                  ],
+                ),
+              ],
             ),
           ),
         );
       },
       separatorBuilder: (context,index){return SizedBox(height: 20,);},
-      itemCount: bookItems.length),
+      itemCount: 2),
 
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue,
         onPressed: (){
           Navigator.push((context), MaterialPageRoute(builder: (context)=>Formpage()));
+          loadBooks();
         },
         child: Icon(Icons.add_outlined,color: Colors.white,),
         ),
